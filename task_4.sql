ad2aa3dff57aa9d909e7216a69d2f11f0fe8e9ee
Elyas <elaman99619@gmail.com>
2025-08-17 11:53:15 +0300

    dml

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Show full description of the table 'books'
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'Books';
