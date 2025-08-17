96be3dd0c3f6fc8eab043b0313acb9d6936a38bb
Elyas <elaman99619@gmail.com>
2025-08-17 11:35:37 +0300

    dml

---
+++ MySQLServer.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+
+def create_database():
+    try:
+         conn = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = '121212',
+        database = 'alx_book_store'
+    )
+
+    if conn.is_connected():
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+    except Error as e:
+        print(f"Error while connecting to MYSQL: {e}")
+    
+    finally:
+        if 'cursor' in locals() and cursor is not None:
+            cursor.close()
+        if 'connection' in locals() and conn.is_connected():
+            conn.close()
+
+if __name__ == "__main__":
+    create_database()
